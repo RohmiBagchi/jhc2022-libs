3ce64b52d4f2bce32e4b35161560041d1c79410b
RohmiBagchi <114489672+RohmiBagchi@users.noreply.github.com>
2022-12-20 21:13:09 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -3,6 +3,6 @@ def call(result){
    string defaultValue: 20, description: 'Enter number two', name: 'varTwo'
          def varOne = params.varOne as int
          def varTwo = params.varTwo as int
-         def result = varOne + varTwo
+         def result = (varOne + varTwo)
                 echo "Result is $result"
    }
