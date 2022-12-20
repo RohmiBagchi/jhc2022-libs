80fcf62c4da024375ea8dd943121fe8b2e8b4ef5
RohmiBagchi <114489672+RohmiBagchi@users.noreply.github.com>
2022-12-20 15:30:19 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,6 +1,8 @@
 def call(result){
    string defaultValue: '10', description: 'Enter number one', name: 'varOne'
    string defaultValue: '20', description: 'Enter number two', name: 'varTwo'
+}
+{
          def varOne = params.varOne as int
                     def varTwo = params.varTwo as int
                     def result = varOne + varTwo
