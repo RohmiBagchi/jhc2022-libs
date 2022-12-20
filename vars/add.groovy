bce42845af1cd65c6087d91e5652912af13d12e8
RohmiBagchi <114489672+RohmiBagchi@users.noreply.github.com>
2022-12-20 14:13:20 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,5 +1,7 @@
-int a = 10;
-  stage('arithmetic stage') {
-    int b = params.Value;
-    c = a + b;
-    echo "${c}
+def call(result)
+string defaultValue: '10', description: 'Enter number one', name: 'varOne'
+string defaultValue: '20', description: 'Enter number two', name: 'varTwo'
+def varOne = params.varOne as int
+def varTwo = params.varTwo as int
+def result = varOne + varTwo
+echo "Result is ${result}"
